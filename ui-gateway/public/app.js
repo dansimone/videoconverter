09562d8185490a06436b035eeb8100864d1109e3
dan.simone@oracle.com <dan.simone@oracle.com>
2016-12-23 12:21:21 -0800

    UI-Gateway - Angular - connection between frontend and backend.

--- ui-gateway/public/app.js
+++ ui-gateway/public/app.js
@@ -1,23 +1,46 @@
 // Define the `phonecatApp` module
 var phonecatApp = angular.module('phonecatApp', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
-phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-  $scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
-  ];
+
+// Define the `VideoListController` controller on the `phonecatApp` module
+phonecatApp.controller('VideoListController', function VideoListController($scope, $http) {
+  $scope.videos = [];
+  refreshVideoStatuses = function () {
+    $http.get('/videos')
+      .success(function (data, status, headers, config) {
+        $scope.videos = data;
+        //console.log("Video data: " + JSON.stringify(data));
+      })
+      .error(function (data, status, header, config) {
+        console.log();
+      });
+  };
+
+  $scope.init = function () {
+    refreshVideoStatuses()
+  };
+
+  $scope.refreshVideoStatuses = function () {
+    refreshVideoStatuses()
+  };
+
+  $scope.getPendingVideos = function () {
+    return $scope.videos.filter(function checkAdult(video) {
+      return video.status == "PENDING";
+    });
+  };
+
+  $scope.getInProgressVideos = function () {
+    return $scope.videos.filter(function checkAdult(video) {
+      return video.status == "IN_PROGRESS";
+    });
+  };
+
+  $scope.getCompletedVideos = function () {
+    return $scope.videos.filter(function checkAdult(video) {
+      return video.status == "COMPLETED";
+    });
+  };
 
   $scope.name = 'world';
 });
--- ui-gateway/server.js
+++ ui-gateway/server.js
@@ -13,6 +13,45 @@ var server = app.listen(getValueOrDefault(process.env.PORT, 8080), function () {
   console.log("Example app listening at http://%s:%s", host, port);
 })
 
+// Gets the list of all videos that haven't started yet
+app.get('/videos', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  videos = [
+    {id: "abc1", name: "Movie" + Math.random(), originalSize: "133MB", status: "PENDING"},
+    {id: "abc2", name: "Movie" + Math.random(), originalSize: "133MB", status: "PENDING"},
+    {id: "abc3", name: "Movie" + Math.random(), originalSize: "133MB", status: "IN_PROGRESS", percentComplete: 66},
+    {id: "abc4", name: "Movie" + Math.random(), originalSize: "133MB", status: "IN_PROGRESS", percentComplete: 24},
+    {id: "abc5", name: "Movie" + Math.random(), originalSize: "133MB", status: "IN_PROGRESS", percentComplete: 97},
+    {id: "abc6", name: "Movie" + Math.random(), originalSize: "133MB", status: "IN_PROGRESS", percentComplete: 0},
+    {
+      id: "abc7",
+      name: "Movie" + Math.random(),
+      originalSize: "133MB",
+      status: "COMPLETED",
+      percentComplete: 100,
+      finalSize: "88MB"
+    },
+    {
+      id: "abc8",
+      name: "Movie" + Math.random(),
+      originalSize: "133MB",
+      status: "COMPLETED",
+      percentComplete: 100,
+      finalSize: "78MB"
+    },
+    {
+      id: "abc9",
+      name: "Movie" + Math.random(),
+      originalSize: "133MB",
+      status: "COMPLETED",
+      percentComplete: 100,
+      finalSize: "43MB"
+    }
+  ];
+  res.send(JSON.stringify(videos));
+})
+
 function getValueOrDefault(value, defaultValue) {
   return value != null ? value : defaultValue;
 }
