65435de269c0beb86809e72b10e3b919a51b99e7
dan.simone@oracle.com <dan.simone@oracle.com>
2017-01-06 16:43:14 -0800

    Use proxy variable other than HTTP_PROXY.

--- ui-gateway/build-docker.sh
+++ ui-gateway/build-docker.sh
@@ -4,5 +4,5 @@ PROJECT_ID="${PROJECT_ID:?Variable PROJECT_ID must be defined}"
 PROJECT_VERSION="${PROJECT_VERSION:?Variable PROJECT_VERSION must be defined}"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build -f $SCRIPT_DIR/Dockerfile --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t \
+docker build -f $SCRIPT_DIR/Dockerfile --build-arg http_proxy=$PROXY --build-arg https_proxy=$PROXY -t \
   gcr.io/$PROJECT_ID/ui-gateway:$PROJECT_VERSION $SCRIPT_DIR
